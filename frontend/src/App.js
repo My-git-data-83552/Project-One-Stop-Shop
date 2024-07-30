11da891b3fb91f26ec1c9040f823d49a281a5160
kd2_shantanu_83552 <shantanu.khilare.16@gmail.com>
2024-07-30 18:47:59 +0530

    project icon uploaded and navbar created

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './components/Navigation_bar';
+
+function App() {
+  return (
+    <div className="App">
+      <navigation_bar />
+    </div>
+  );
+}
+// 
+export default App;
